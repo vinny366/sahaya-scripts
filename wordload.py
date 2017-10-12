7301b3e50bb60422024cc2377be0bea838a840e8
Vineel Vutukuri <vvutukur@asu.edu>
2017-10-11 18:45:11 -0700

    Update wordload.py

--- wordload.py
+++ wordload.py
@@ -6,7 +6,8 @@ def printwords(length, letter):
 	
 
 if __name__ == '__main__':
-	words = pickle.load(open( "worddict.pkl", "rb" ))
-	length = raw_input('Enter the length of the words\n')
-	letter = raw_input('Enter a character\n')
-	printwords(length, letter)
+	words = pickle.load(open( "worddict.pkl", "rb" ));
+	length = raw_input('Enter the length of the words\n');
+	letter = raw_input('Enter a character\n');
+	printwords(length, letter);
+	print("Successfully completed printing");
